3e6602e1f407b6283a6b933ee4021a2c5fc5c92b
lsollis <92566497+lsollis@users.noreply.github.com>
2025-08-14 12:42:32 -1000

    basic nvflare train functioning

---
+++ start-jupyter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /workspace
+jupyter notebook --ip=0.0.0.0 --port=8887 --no-browser --allow-root
+~                                                                               
+~                                                                        
